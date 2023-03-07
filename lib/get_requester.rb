be528c47238329e909dc9deeeab8ed65905c735f
Brian Wahungu <cepteinhawks@gmail.com>
2023-03-07 08:07:18 +0300

    only one test passing

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,18 @@
-# Write your code here
+require 'net/http'
+require 'json'
+
+class GetRequester
+  def initialize(url)
+    @url = url
+  end
+
+  def get_data
+    uri = URI(@url)
+    response = Net::HTTP.get(uri)
+    JSON.parse(response)
+  end
+end
+
+requester = GetRequester.new('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+data = requester.get_data
+puts data
